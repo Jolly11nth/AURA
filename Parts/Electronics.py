911b717ec5caba053704d436e16c1f2c4918561f
Jolly Christopher <jollychris131@gmail.com>
2026-09-14 12:35:00 +0100

    Route electronics placement through Geometry authority

--- Parts/Electronics.py
+++ Parts/Electronics.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 from dataclasses import dataclass
 from typing import Any
 
+from Core.Geometry import GeometryEngine, Point3D
 from Core.Parameters import AURAParameters, DEFAULT_PARAMETERS
 
 
@@ -21,17 +22,36 @@ class ElectronicsPart:
         e = self.parameters.electronics
         return e.battery_bay_length_mm, e.battery_bay_width_mm, self.parameters.tray.depth_mm
 
+    @property
+    def controller_center(self) -> Point3D:
+        return GeometryEngine.placement_solver(self.parameters).motherboard_center()
+
+    @property
+    def battery_center(self) -> Point3D:
+        return GeometryEngine.placement_solver(self.parameters).battery_center()
+
     def build_shapes(self) -> tuple[Any, Any]:
         try:
             import Part  # type: ignore[import-not-found]
         except ImportError as exc:
             raise RuntimeError("FreeCAD Part is required to build CAD geometry") from exc
-        e = self.parameters.electronics
-        t = self.parameters.tray
-        body = self.parameters.envelope
-        z = body.ground_clearance_mm + self.parameters.base.floor_thickness_mm + e.service_clearance_mm
-        cx = (body.length_mm - e.controller_bay_length_mm - e.battery_bay_length_mm) / 3
-        controller = Part.makeBox(e.controller_bay_length_mm, e.controller_bay_width_mm, t.depth_mm, Part.Vector(cx, (body.width_mm - e.controller_bay_width_mm) / 2, z))
-        battery_x = cx + e.controller_bay_length_mm + cx
-        battery = Part.makeBox(e.battery_bay_length_mm, e.battery_bay_width_mm, t.depth_mm, Part.Vector(battery_x, (body.width_mm - e.battery_bay_width_mm) / 2, z))
+
+        controller = self._box_from_center(self.controller_center, self.controller_dimensions)
+        battery = self._box_from_center(self.battery_center, self.battery_dimensions)
         return controller, battery
+
+    @staticmethod
+    def _box_from_center(center: Point3D, dimensions: tuple[float, float, float]) -> Any:
+        import Part
+
+        length_mm, width_mm, height_mm = dimensions
+        return Part.makeBox(
+            length_mm,
+            width_mm,
+            height_mm,
+            Part.Vector(
+                center.x_mm - (length_mm / 2.0),
+                center.y_mm - (width_mm / 2.0),
+                center.z_mm - (height_mm / 2.0),
+            ),
+        )
